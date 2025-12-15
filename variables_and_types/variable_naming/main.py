6394ab8b2a4a176940c995d1b788186b178b5daa
bschaub805-Dover15986 <>
2025-12-15 03:55:44 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,7 @@
+item_name = "Bread"
+Item_price = 4.52
+items_in_stock = 230
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", Item_price)
+print("Quantity in stock:", items_in_stock )
